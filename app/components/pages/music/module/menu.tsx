b695ed82b747324eb571a4e2f6c53ee5f1232200
Tanapong Saramon <>
2024-01-13 18:39:50 +0700

    music check complete

--- app/components/pages/music/module/menu.tsx
+++ app/components/pages/music/module/menu.tsx
@@ -1,16 +1,10 @@
 import {
   Menubar,
-  MenubarCheckboxItem,
   MenubarContent,
   MenubarItem,
   MenubarMenu,
-  MenubarRadioGroup,
-  MenubarRadioItem,
   MenubarSeparator,
   MenubarShortcut,
-  MenubarSub,
-  MenubarSubContent,
-  MenubarSubTrigger,
   MenubarTrigger,
 } from "~/components/ui/menubar";
 export type Menu = {
@@ -92,6 +86,33 @@ export default function PagesMusicModuleMenu() {
         { name: "Find in Music", shortcut: "⌥⌘F" },
         { name: "Find in Playlist", shortcut: "⌥⌘F" },
         { name: "Find in Library", shortcut: "" },
+      ],
+    },
+    {
+      title: "View",
+      menu: [
+        { name: "Show Status Bar", shortcut: "⌘/" },
+        { name: "Show Artwork", shortcut: "⌘/" },
+        { name: "Show Column Browser", shortcut: "⌘/" },
+        { name: "Show Visualizer", shortcut: "⌘/" },
+        { name: "Show Equalizer", shortcut: "⌘/" },
+        { name: "Show Lyrics Viewer" },
+      ],
+    },
+    {
+      title: "Account",
+      menu: [
+        { name: "Sign In", shortcut: "⌘/" },
+        { name: "Sign Out", shortcut: "⌘/" },
+        { name: "View My Account", shortcut: "⌘/" },
+        { name: "View My Profile", shortcut: "⌘/" },
+        { name: "View My Apple ID", shortcut: "⌘/" },
+        { name: "View My iCloud Music Library", shortcut: "⌘/" },
+        { name: "View My Subscriptions", shortcut: "⌘/" },
+        { name: "View My Devices", shortcut: "⌘/" },
+        { name: "View My Home Videos", shortcut: "⌘/" },
+        { name: "View My Wish List", shortcut: "⌘/" },
+        { name: "View My Pre-Orders", shortcut: "⌘/" },
       ],
     },
   ];
